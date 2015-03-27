6cc3004054eb04fef890a8006caf573bc5323b81
laurenbenichou <laurenbenichou@gmail.com>
2015-03-27 16:17:18 -0700

    added all icon

--- js/app.js
+++ js/app.js
@@ -4,95 +4,216 @@ L.mapbox.accessToken = 'pk.eyJ1IjoibGF1cmVuYmVuaWNob3UiLCJhIjoiQ1BlZGczRSJ9.EVMi
 
 // Create a map in the div #map and takes two arguments: the id of the html element and the map id from Mapbox
 var map = L.mapbox.map('map', 'laurenbenichou.54e91cf8', {
-	zoomControl: false
+  zoomControl: false
 }).setView([37.812, -122.294], 15);
 
 map.scrollWheelZoom.disable();
 
-var places = { type: 'FeatureCollection', features: [
-    { geometry: { type: "Point", coordinates: [37.812, -122.294] },
-      properties: { id: "cover", zoom: 15 }, type: 'Feature' },
-    { geometry: { type: "Point", coordinates: [37.812515, -122.287030] },
-      properties: { id: "defermery" }, type: 'Feature' },
-    { geometry: { type: "Point", coordinates: [37.805054, -122.295128] },
-      properties: { id: "bart" }, type: 'Feature' },
-    { geometry: { type: "Point", coordinates: [37.815969, -122.316791] },
-      properties: { id: "crane"}, type: 'Feature' },
-    { geometry: { type: "Point", coordinates: [37.815694, -122.297240] },
-      properties: { id: "station" }, type: 'Feature' },
-    { geometry: { type: "Point", coordinates: [37.808917, -122.292539] },
-      properties: { id: "mural" }, type: 'Feature' },
-    { geometry: { type: "Point", coordinates: [37.826317, -122.278605] },
-      properties: { id: "hotel"}, type: 'Feature' },
-    { geometry: { type: "Point", coordinates: [37.818513, -122.278765] },
-      properties: { id: "mcclymonds" }, type: 'Feature' }
-    ]};
+var places = {
+  type: 'FeatureCollection',
+  features: [{
+    geometry: {
+      type: "Point",
+      coordinates: [37.812, -122.294]
+    },
+    properties: {
+      id: "cover",
+      zoom: 15
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.812515, -122.287030]
+    },
+    properties: {
+      id: "defermery"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.805054, -122.295128]
+    },
+    properties: {
+      id: "bart"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.815969, -122.316791]
+    },
+    properties: {
+      id: "crane"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.815694, -122.297240]
+    },
+    properties: {
+      id: "station"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.808917, -122.292539]
+    },
+    properties: {
+      id: "mural"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.826317, -122.278605]
+    },
+    properties: {
+      id: "hotel"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.818513, -122.278765]
+    },
+    properties: {
+      id: "mcclymonds"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.818513, -122.278765]
+    },
+    properties: {
+      id: "malik"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.811607, -122.294480]
+    },
+    properties: {
+      id: "cookie"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.812878, -122.294654]
+    },
+    properties: {
+      id: "kevin"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.810850, -122.296330]
+    },
+    properties: {
+      id: "open"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.809877, -122.294366]
+    },
+    properties: {
+      id: "joshua"
+    },
+    type: 'Feature'
+  }, {
+    geometry: {
+      type: "Point",
+      coordinates: [37.807101, -122.269838]
+    },
+    properties: {
+      id: "about"
+    },
+    type: 'Feature'
+  }]
+};
 
- var spots = L.mapbox.featureLayer()
-    .addTo(map);
+var spots = L.mapbox.featureLayer()
+  .addTo(map);
 
-  spots.setGeoJSON(places);
+spots.setGeoJSON(places);
 
-// define icons as cssIcons 
-spots.eachLayer(function(e){
-    var className = 'sprite sprite-' + e.feature.properties.id;
-    var coordinates = e.feature.geometry.coordinates;
-    var cssIcon = L.divIcon({
-      // Specify a class name we can refer to in CSS.
-      className: className,
-      // Set marker width and height
-      iconSize: [80, 80]
-    });
-    L.marker(coordinates, {icon: cssIcon}).addTo(map);
+// define icons as cssIcons
+spots.eachLayer(function(e) {
+  var className = 'sprite sprite-' + e.feature.properties.id;
+  var coordinates = e.feature.geometry.coordinates;
+  var cssIcon = L.divIcon({
+    // Specify a class name we can refer to in CSS.
+    className: className,
+    // Set marker width and height
+    iconSize: [80, 80]
   });
+  L.marker(coordinates, {
+    icon: cssIcon
+  }).addTo(map);
+});
 
 // Array of story section elements.
 var sections = $('section');
 var narrative = $("#narrative")[0];
 var currentId = 'cover';
 
-function setId(newId){
+function setId(newId) {
   // If ID hasn't change, do nothing
   if (newId === currentId) return;
+  if (newId === 'cover') {
+    console.log(newId)
+    $('body').attr('class', 'section-0');
+  } else {
+    $('body').attr('class', ' ');
+  }
   // otherwise, iterate thru layers, setting the current marker to a different style and zooming to it
-  spots.eachLayer(function(layer){
-    if(layer.feature.properties.id === newId){
+  spots.eachLayer(function(layer) {
+    if (layer.feature.properties.id === newId) {
       var coordinates = layer.feature.geometry.coordinates;
       var all_el = $(".sprite");
       all_el.removeClass("active");
       var el = $("div[class*='" + newId + "']")[0];
       el.className = el.className + " active";
-      map.setView(coordinates, 16);
+      if (newId === "cover") {
+        map.setView(coordinates, 15);
+      } else {
+        map.setView(coordinates, 16);
+      }
+
     }
   });
   // highlight the current section
-   for (var i = 0; i < sections.length; i++) {
-        sections[i].className = sections[i].id === newId ? 'active' : '';
-    }
-    // And then set the new id as the current one,
-    // so that we know to do nothing at the beginning
-    // of this function if it hasn't changed between calls
-    currentId = newId;
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].className = sections[i].id === newId ? 'active' : '';
+  }
+  // And then set the new id as the current one,
+  // so that we know to do nothing at the beginning
+  // of this function if it hasn't changed between calls
+  currentId = newId;
 }
 
 
 narrative.onscroll = function() {
-    var narrativeHeight = narrative.offsetHeight;
-    var newId = currentId;
-    if (newId === 'cover'){
-      $('body').attr('class', 'section-0');
-    }else{
-      $('body').attr('class', ' ');
+  var narrativeHeight = narrative.offsetHeight;
+  var newId = currentId;
+  // Find the section that's currently scrolled-to.
+  // We iterate backwards here so that we find the topmost one.
+  for (var i = sections.length - 1; i >= 0; i--) {
+    var rect = sections[i].getBoundingClientRect();
+    if (rect.top >= 0 && rect.top / .5 <= narrativeHeight) {
+      newId = sections[i].id;
     }
-    // Find the section that's currently scrolled-to.
-    // We iterate backwards here so that we find the topmost one.
-    for (var i = sections.length - 1; i >= 0; i--) {
-        var rect = sections[i].getBoundingClientRect();
-        if (rect.top >= 0 && rect.top/.5 <= narrativeHeight) {
-            newId = sections[i].id;
-        }
-    }
-    setId(newId);
+  }
+  setId(newId);
 };
 
 setId('cover');
@@ -101,24 +222,24 @@ setId('cover');
 
 /* fix vertical when not overflow
 call fullscreenFix() if .fullscreen content changes */
-function fullscreenFix(){
-    var h = $('body').height();
-    // set .fullscreen height
-    $(".content-b").each(function(){
-        if($(this).innerHeight() <= h){
-            $(this).closest(".fullscreen").addClass("not-overflow");
-        }
-    });
+function fullscreenFix() {
+  var h = $('body').height();
+  // set .fullscreen height
+  $(".content-b").each(function() {
+    if ($(this).innerHeight() <= h) {
+      $(this).closest(".fullscreen").addClass("not-overflow");
+    }
+  });
 }
 
 
 $(window).resize(fullscreenFix);
 fullscreenFix();
 
-function fadeOnScroll(){
-    var target = $("#splash");
-    $(window).scroll(function (){
-        target.fadeOut(1500);
-    });
+function fadeOnScroll() {
+  var target = $("#splash");
+  $(window).scroll(function() {
+    target.fadeOut(1500);
+  });
 }
 fadeOnScroll();
