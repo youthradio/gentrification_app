45e8d839def32ddc432e266cd47699ee0d7f4c70
laurenbenichou <laurenbenichou@gmail.com>
2015-04-21 11:18:19 -0700

    added error message for 405 error

--- mailer.php
+++ mailer.php
@@ -1,62 +1,64 @@
 <?php
 
     // Only process POST requests.
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-    $name = strip_tags(trim($_POST["first_name"]));
-    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $name = strip_tags(trim($_POST["first_name"]));
+        $name = str_replace(array("\r","\n"),array(" "," "),$name);
 
-    $last_name = strip_tags(trim($_POST["last_name"]));
-    $last_name = str_replace(array("\r","\n"),array(" "," "),$last_name);
+         $last_name = strip_tags(trim($_POST["last_name"]));
+        $last_name = str_replace(array("\r","\n"),array(" "," "),$last_name);
 
-    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $story = trim($_POST["story"]);
-    $location = trim($_POST["location"]);
-    $image_url = trim($_POST["image_url"]);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $story = trim($_POST["story"]);
+        $location = trim($_POST["location"]);
+        $image_url = trim($_POST["image_url"]);
 
         // Check that data was sent to the mailer.
-    if ( empty($name) OR empty($story) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($story) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
-        http_response_code(400);
-        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-        exit;
-    }
+            http_response_code(400);
+            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            exit;
+        }
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-    $recipient = "lo@youthradio.org";
+        $recipient = "lo@youthradio.org";
 
         // Set the email subject.
-    $subject = "West Side Stories: new contact from $name";
+        $subject = "West Side Stories: new contact from $name";
 
         // Build the email content.
-    $email_content = "Name/Nickname: $name\n";
-    $email_content .= "Last Name: $last_name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Story:\n$story\n";
-    $email_content .= "Location:\n$location\n";
-    $email_content .= "Image Url:\n$image_url\n";
+        $email_content = "Name/Nickname: $name\n";
+        $email_content .= "Last Name: $last_name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Story:\n$story\n";
+        $email_content .= "Location:\n$location\n";
+        $email_content .= "Image Url:\n$image_url\n";
 
         // Build the email headers.
-    $email_headers = "From: $name <$email>";
+        $email_headers = "From: $name <$email>";
 
         // Send the email.
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
-        http_response_code(200);
-        echo "Thank You! Your message has been sent.";
-    } else {
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
             // Set a 500 (internal server error) response code.
-        http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
-    }else {
-        // set a 405 (not allowed) response code.
-    http_response_code(405);
-    echo "There was an error. Please try again later.";
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
         }
-} else {
+
+    } else {
         // Not a POST request, set a 403 (forbidden) response code.
-    http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
-}
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+
+    if(http_response_code(405)){
+        echo "There was a problem with your submission, please try again.";
+    }
+
 ?>
